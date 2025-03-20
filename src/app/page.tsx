253076127596eac0d2f8d2b5f269f01cc7d382df
Raka Fantino <rakafantino@gmail.com>
2025-03-20 12:09:03 +0700

    Perbarui komponen ScrollIndicator dengan menambahkan gaya font dan meningkatkan ukuran ikon. Sesuaikan jalur SVG untuk memperbaiki tampilan panah.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -162,19 +162,19 @@ const ScrollIndicator = ({ show }: { show: boolean }) => {
   return (
     <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 z-20 hidden md:block">
       <div className="flex flex-col items-center scroll-indicator">
-        <span className="text-white text-sm mb-2 ">Scroll Down</span>
+        <span className="text-white text-sm mb-2 font-dion tracking-wide">Scroll Down</span>
         <svg 
           xmlns="http://www.w3.org/2000/svg" 
-          className="h-6 w-6 text-white" 
+          className="h-8 w-8 text-white" 
           fill="none" 
           viewBox="0 0 24 24" 
           stroke="currentColor"
+          strokeWidth={2}
         >
           <path 
             strokeLinecap="round" 
             strokeLinejoin="round" 
-            strokeWidth={2} 
-            d="M19 14l-7 7m0 0l-7-7m7 7V3" 
+            d="M19 14l-7 7m0 0l-7-7m7 7C12 12 12 8 12 3"
           />
         </svg>
       </div>
